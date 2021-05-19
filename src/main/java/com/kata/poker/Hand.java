905cfb1562ad2d4eecace27a1217d84028c8da98
Yehouda <yehouda_arnauve@hotmail.com>
2021-05-19 18:44:31 +0200

    refactoring: evaluate two pair

--- src/main/java/com/kata/poker/Hand.java
+++ src/main/java/com/kata/poker/Hand.java
@@ -1,6 +1,7 @@
 package com.kata.poker;
 
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Hand {
@@ -11,34 +12,32 @@ public class Hand {
     }
 
     public String evaluate() {
-        List<Integer> values = this.handGame.stream().map(Card::getValue).collect(Collectors.toList());
-        List<Integer> paires = values.stream()
-                .filter(val -> Collections.frequency(values, val) == 2)
-                .distinct()
-                .collect(Collectors.toList());
-        if (paires.size() == 2) {
-            List<String> pairesString = new ArrayList<>(2);
-            for (int pair : paires) {
-                pairesString.add(this.handGame.stream()
-                        .filter(c -> c.getValue() == pair).map(Card::getCardName).findFirst().orElse(null));
-            }
-            return "two pair of : " + String.join(" and ", pairesString);
+        List<Integer> paires = getPaires();
+        List<Card> cardPair = paires.stream().map(this::getCardPair).collect(Collectors.toList());
+        if (cardPair.size() == 2) {
+            return "two pair of : " + cardPair.stream().map(Card::getCardName).collect(Collectors.joining(" and "));
         }
-        Card cardPair = evaluateCardPair();
-        if (cardPair != null) {
-            return "pair of : " + cardPair.getCardName();
+        if (!cardPair.isEmpty()) {
+            return "pair of : " + cardPair.get(0).getCardName();
         }
         Card highCard = evaluateHighCard();
         return "high card: " + highCard.getCardName();
     }
 
-    private Card evaluateCardPair() {
+    private Card getCardPair(int paireValue) {
+        return this.handGame.stream()
+                .filter(c -> c.getValue() == paireValue).findFirst().orElse(null);
+    }
+
+    private List<Integer> getPaires() {
         List<Integer> values = this.handGame.stream().map(Card::getValue).collect(Collectors.toList());
-        Optional<Integer> optionalPairValue = values.stream()
-                .filter(val -> Collections.frequency(values, val) == 2).findFirst();
-        return optionalPairValue.isEmpty() ? null :
-                this.handGame.stream()
-                        .filter(c -> c.getValue() == optionalPairValue.get()).findFirst().orElse(null);
+        List<Integer> paires = values.stream()
+                .filter(frequencyOfCardValuePredicate(values, 2)).distinct().collect(Collectors.toList());
+        return paires;
+    }
+
+    private Predicate<Integer> frequencyOfCardValuePredicate(List<Integer> values, int numberOfFrequency) {
+        return val -> Collections.frequency(values, val) == numberOfFrequency;
     }
 
     private Card evaluateHighCard() {
